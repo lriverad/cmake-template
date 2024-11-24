8f643fe5f4a84ba00c40f449c2bcc044cd2ca97a
lr1609 <1609lar@gmail.com>
2024-11-24 12:21:51 -0900

    added wasm support via emscripten

--- src/main.c
+++ src/main.c
@@ -1,13 +1,40 @@
-#include <stdio.h>
-
-int main(int argc, char **argv) {
-  printf("Hello, C!\n");
-
-#ifdef NDEBUG
-  printf("Release build...\n");
-#elif defined (DEBUG)
-  printf("Debug build...\n");
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
 #endif
 
-  return 0;
+#include <stdbool.h>
+#include <stdio.h>
+
+bool is_running = true;
+
+void mainloop(void) {
+#ifdef __EMSCRIPTEN__
+    if (!is_running) {
+        // Reload app
+    }
+#endif
+}
+
+int main(int argc, char **argv) {
+    printf("Hello, C!\n");
+
+#ifdef __EMSCRIPTEN__
+    printf("Hello, Emscripten!\n");
+#endif
+
+#ifdef NDEBUG
+    printf("Running release build...\n");
+#elif defined(DEBUG)
+    printf("Running debug build...\n");
+#endif
+
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(mainloop, 60, 1);
+#else
+    while (is_running) {
+        mainloop();
+    }
+#endif
+
+    return 0;
 }
